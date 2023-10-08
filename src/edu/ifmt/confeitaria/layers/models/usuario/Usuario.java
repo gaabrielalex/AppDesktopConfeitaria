0c7c67a5fd285c46849d834da4e37f4be78f0f39
Gabriel <gabrielalexsilva2@gmail.com>
2023-10-08 17:15:45 -0400

    .

--- src/edu/ifmt/confeitaria/layers/models/usuario/Usuario.java
+++ src/edu/ifmt/confeitaria/layers/models/usuario/Usuario.java
@@ -1,28 +1,20 @@
 package edu.ifmt.confeitaria.layers.models.usuario;
 
-public class Usuario {
-    private Long idUsuario;
+public class Usuario extends SuperModel{
     private String nome;
     private String login;
     private String senha;
 
     /*Construtor vazio para configurações do DatabaseAcessComponentManager*/
     public Usuario() {
+        super(null);
     }
    
-    public Usuario(Long idUsuario, String nome, String login, String senha) {
-        this.idUsuario = idUsuario;
+    public Usuario(Long ID, String nome, String login, String senha) {
+        super(ID);
         this.nome = nome;
         this.login = login;
         this.senha = senha;
-    }
-
-    public Long getIdUsuario() {
-        return idUsuario;
-    }
-
-    public void setIdUsuario(Long idUsuario) {
-        this.idUsuario = idUsuario;
     }
 
     public String getNome() {
@@ -47,30 +39,5 @@ public class Usuario {
 
     public void setSenha(String senha) {
         this.senha = senha;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((idUsuario == null) ? 0 : idUsuario.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Usuario other = (Usuario) obj;
-        if (idUsuario == null) {
-            if (other.idUsuario != null)
-                return false;
-        } else if (!idUsuario.equals(other.idUsuario))
-            return false;
-        return true;
     }
 }
