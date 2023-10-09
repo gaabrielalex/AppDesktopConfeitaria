a73f1c1f71703ecb29c7b183a204168393716207
Gabriel <gabrielalexsilva2@gmail.com>
2023-10-08 22:20:41 -0400

    .

--- src/edu/ifmt/confeitaria/util/abstraction_classes/DatabaseAccessComponentManager.java
+++ src/edu/ifmt/confeitaria/util/abstraction_classes/DatabaseAccessComponentManager.java
@@ -12,7 +12,6 @@ import java.util.function.Supplier;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.JToggleButton;
