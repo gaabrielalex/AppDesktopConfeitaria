bbb01984a78f1164ec7c20029b93bb44757e34d1
Gabriel <gabrielalexsilva2@gmail.com>
2023-10-11 13:38:15 -0400

    .

--- src/edu/ifmt/confeitaria/util/abstraction_classes/SuperService.java
+++ src/edu/ifmt/confeitaria/util/abstraction_classes/SuperService.java
@@ -3,8 +3,8 @@ package edu.ifmt.confeitaria.util.abstraction_classes;
 import java.util.List;
 
 public abstract class SuperService<T> {
-    public abstract List<T> select();
-    public abstract List<T> remakeLastSelect();
+    public abstract List<T> selectAll();
+    public abstract List<T> remakeLastPartialSearch();
     public abstract boolean insert(T object);
     public abstract boolean update(T object, T originalObject); 
 }
