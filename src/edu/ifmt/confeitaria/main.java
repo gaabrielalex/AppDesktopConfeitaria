b7e41d3954e88d70642c80af86768ce51b44f3d5
Gabriel <gabrielalexsilva2@gmail.com>
2023-10-11 11:24:45 -0400

    .

--- src/edu/ifmt/confeitaria/main.java
+++ src/edu/ifmt/confeitaria/main.java
@@ -1,27 +1,15 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package edu.ifmt.confeitaria;
 
 import edu.ifmt.confeitaria.layers.controllers.authentication.AutenticacaoController;
 import edu.ifmt.confeitaria.util.themes.NimbusTheme;
 
-/**
- *
- * @author Gabriel
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         //Definindo o tema da aplicação
         NimbusTheme.setTheme();
         
         //Iniciando a interface de start da aplicação: interface de login
-        new AutenticacaoController().displayView();
-     
+        AutenticacaoController.getInstance().displayView();
     }
 }
