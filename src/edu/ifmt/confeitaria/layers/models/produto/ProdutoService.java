e19a03918d9162f8c939b8017872291833619b7e
Gabriel <gabrielalexsilva2@gmail.com>
2023-11-03 01:11:09 -0400

    .

--- src/edu/ifmt/confeitaria/layers/models/produto/ProdutoService.java
+++ src/edu/ifmt/confeitaria/layers/models/produto/ProdutoService.java
@@ -1,5 +1,8 @@
 package edu.ifmt.confeitaria.layers.models.produto;
 
-public class ProdutoService {
+import edu.ifmt.confeitaria.layers.models.cliente.Cliente;
+import edu.ifmt.confeitaria.util.abstraction_classes.SuperService;
+
+public class ProdutoService extends SuperService<Produto> {
     
 }
