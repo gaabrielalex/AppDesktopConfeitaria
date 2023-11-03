629d1abb6da6f4ac43310be06f1852e95a5c333c
Gabriel <gabrielalexsilva2@gmail.com>
2023-11-03 01:07:33 -0400

    .

--- src/edu/ifmt/confeitaria/layers/models/produto/ProdutoDAO.java
+++ src/edu/ifmt/confeitaria/layers/models/produto/ProdutoDAO.java
@@ -150,40 +150,40 @@ public class ProdutoDAO {
         }
     }
 
-    // public boolean update(Cliente cliente, Long originalID) {
-    //     if(originalID == null) return false;
-    //     PreparedStatement statement = null;
+    public boolean update(Produto produto, Long originalID) {
+        if(originalID == null) return false;
+        PreparedStatement statement = null;
 
-    //     //Cria a query
-    //         String sql =    "UPDATE cliente " +
-    //                         "SET id_produto = ?, nome = ?, cpf = ?, telefones = ?, endereco = ?, link_endereco = ? " +
-    //                             "WHERE id_produto = ?";
+        //Cria a query
+            String sql =    "UPDATE produto " +
+                            "SET id_produto = ?, descricao = ?, vlr_unitario = ?, observacoes = ?," +
+                                "id_tipo_chocolate = (select id_tipo_chocolate from tipo_chocolate where descricao = ?) " +
+                            "WHERE id_produto = ?";
 
-    //     try {
-    //         //Define o PreparedStatement com o SQL, em seguida, configura os parâmetros necessários
-    //         statement = DBConnection.getConnection().prepareStatement(sql);
-    //         statement.setLong(1, cliente.getID());
-    //         statement.setString(2, cliente.getNome());
-    //         statement.setString(3, cliente.getCPF());
-    //         statement.setString(4, cliente.getTelefones());
-    //         statement.setString(5, cliente.getEndereco());
-    //         statement.setString(6, cliente.getLinkEndereco());
-    //         statement.setLong(7, originalID);
+        try {
+            //Define o PreparedStatement com o SQL, em seguida, configura os parâmetros necessários
+            statement = DBConnection.getConnection().prepareStatement(sql);
+            statement.setLong(1, produto.getID());
+            statement.setString(2, produto.getDescricao());
+            statement.setBigDecimal(3, produto.getVlrUnitario());
+            statement.setString(4, produto.getObservacoes());
+            statement.setString(5, produto.getTipoChocolate());
+            statement.setLong(6, originalID);
             
-    //         //Executa a query
-    //         statement.executeUpdate();
+            //Executa a query
+            statement.executeUpdate();
 
-    //         //Se a atualização foi realizada com sucesso, retorna true
-    //         return true;
-    //     } catch (SQLException e) {
-    //         e.printStackTrace();
-    //         //Se a atualização não foi realizada com sucesso, retorna false
-    //         return false;
-    //     } finally {
-    //         //Fecha a conexão com o banco de dados e os recursos criados a partir dela
-    //         DBConnection.closeConnection(statement);
-    //     }
-    // }
+            //Se a atualização foi realizada com sucesso, retorna true
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            //Se a atualização não foi realizada com sucesso, retorna false
+            return false;
+        } finally {
+            //Fecha a conexão com o banco de dados e os recursos criados a partir dela
+            DBConnection.closeConnection(statement);
+        }
+    }
 
     // public boolean delete(Long idProduto) {
     //     if(idProduto == null) return false;
