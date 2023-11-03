48a60beb2d69a9ac491a49eba79cb59f185b4d1b
Gabriel <gabrielalexsilva2@gmail.com>
2023-11-03 00:32:26 -0400

    .

--- src/edu/ifmt/confeitaria/layers/models/produto/Produto.java
+++ src/edu/ifmt/confeitaria/layers/models/produto/Produto.java
@@ -1,7 +1,26 @@
 package edu.ifmt.confeitaria.layers.models.produto;
 
+import java.math.BigDecimal;
+
 import edu.ifmt.confeitaria.util.abstraction_classes.SuperModel;
 
 public class Produto extends SuperModel {
-    
+    private String descricao;
+    private BigDecimal VlrUnitario;
+    private String observacoes;
+    private String tipo_chocolate;
+
+    /* Construtor vazio para configurações do DatabaseAcessComponentManager */
+    public Produto() {
+        super(null);
+    }
+
+    public Produto(Long ID, String descricao, BigDecimal vlrUnitario, String observacoes, String tipo_chocolate) {
+        super(ID);
+        this.descricao = descricao;
+        VlrUnitario = vlrUnitario;
+        this.observacoes = observacoes;
+        this.tipo_chocolate = tipo_chocolate;
+    }
+
 }
