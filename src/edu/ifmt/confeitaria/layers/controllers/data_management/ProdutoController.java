8527fa752f020097d934f08a16ce94fab304e194
Gabriel <gabrielalexsilva2@gmail.com>
2023-11-03 02:06:48 -0400

    .

--- src/edu/ifmt/confeitaria/layers/controllers/data_management/ProdutoController.java
+++ src/edu/ifmt/confeitaria/layers/controllers/data_management/ProdutoController.java
@@ -4,28 +4,37 @@
  */
 package edu.ifmt.confeitaria.layers.controllers.data_management;
 
+import java.awt.Component;
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+
 import javax.swing.JFrame;
 
+import edu.ifmt.confeitaria.layers.models.cliente.Cliente;
+import edu.ifmt.confeitaria.layers.models.produto.Produto;
+import edu.ifmt.confeitaria.layers.models.produto.ProdutoService;
 import edu.ifmt.confeitaria.layers.views.data_management.ProdutoView;
 import edu.ifmt.confeitaria.util.abstraction_classes.DatabaseAccessComponentManager;
 import edu.ifmt.confeitaria.util.abstraction_classes.SuperController;
 
-/**
- *
- * @author Gabriel
- */
-public class ProdutoController extends SuperController {
+public class ProdutoController extends SuperController<Produto> {
     private final ProdutoView produtoView;
-    private DatabaseAccessComponentManager produtoDBCManager;
+    private final ProdutoService produtoService;
+    private DatabaseAccessComponentManager<Produto> produtoDBCManager;
     
-    public ProdutoController(JFrame previousView, DatabaseAccessComponentManager produtoDBCManager) {
+    public ProdutoController(JFrame previousView, ProdutoService produtoService, DatabaseAccessComponentManager<Produto> produtoDBCManager) {
         //Injeção de dependências
         this.produtoDBCManager = produtoDBCManager;
+        this.produtoService = produtoService;
 
         //Instancia a View e define esta instância como seu controlador
         this.produtoView = new ProdutoView(this, previousView);
 
         //Configurando o DatabaseAccessComponentManager
+        List<Component> fields = Arrays.asList(produtoView.getEdtDescricao(), produtoView.getEdtObs(), 
+            produtoView.getEdtVlrUnt(), produtoView.getCmbTipoChoc());
+        this.produtoDBCManager.setFields(fields);
         this.produtoDBCManager.configureComponents(null, this, null, produtoView.getBtnInsert(),
             produtoView.getBtnUpdate(), produtoView.getBtnDelete(), produtoView.getBtnPost(),
             produtoView.getBtnCancel(), produtoView.getBtnRefresh(), produtoView.getTblProduto());
@@ -46,21 +55,38 @@ public class ProdutoController extends SuperController {
         this.displayView();
     }
 
-    @Override
-    public Object[] modelToTableRow(Object object) {
-        // TODO Auto-generated method stub
-       return null;
+   public void partialSearch(String descricao, String tipoChocolate) {
+        this.produtoDBCManager.setTemporaryTDataList(this.produtoService.partialSearch(descricao, tipoChocolate));
     }
 
     @Override
-    public void modelToFields(Object object) {
-        // TODO Auto-generated method stub
+    public Object[] modelToTableRow(Produto produto) {
+        return new Object[]{
+            produto.getID(),
+            produto.getDescricao(),
+            produto.getTipoChocolate(),
+            produto.getVlrUnitario(),
+            produto.getObservacoes()
+        };
     }
 
     @Override
-    public Object fieldsToModel() {
-        // TODO Auto-generated method stub
-        return null;
+    public void modelToFields(Produto produto) {
+        this.produtoView.getEdtDescricao().setText(produto.getDescricao());
+        this.produtoView.getEdtObs().setText(produto.getObservacoes());
+        this.produtoView.getEdtVlrUnt().setText(produto.getVlrUnitario().toString());
+        this.produtoView.getCmbTipoChoc().setSelectedItem(produto.getTipoChocolate());
+    }
+
+    @Override
+    public Produto fieldsToModel() {
+        return new Produto(
+            null,
+            this.produtoView.getEdtDescricao().getText(),
+            new BigDecimal(this.produtoView.getEdtVlrUnt().getText()),
+            this.produtoView.getEdtObs().getText(),
+            this.produtoView.getCmbTipoChoc().getSelectedItem().toString()
+        );
     }
 
 }
