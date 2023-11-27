fbe203f07502ee8cfb944322ad1ff1ca889ffc75
Gabriel <106288896+gaabrielalex@users.noreply.github.com>
2023-11-27 14:02:17 -0400

    .

--- src/edu/ifmt/confeitaria/layers/models/pedido/PedidoService.java
+++ src/edu/ifmt/confeitaria/layers/models/pedido/PedidoService.java
@@ -1,5 +1,58 @@
 package edu.ifmt.confeitaria.layers.models.pedido;
 
-public class PedidoService {
+import java.util.List;
+
+import edu.ifmt.confeitaria.layers.models.produto.Produto;
+import edu.ifmt.confeitaria.layers.models.produto.ProdutoDAO;
+import edu.ifmt.confeitaria.util.abstraction_classes.SuperService;
+
+public class PedidoService extends SuperService<Pedido> {
+    private final PedidoDAO pedidoDAO;
+
+    public PedidoService(PedidoDAO pedidoDAO) {
+        this.pedidoDAO = pedidoDAO;
+    }
+
+    /* ----- Métodos principais de manipulação de dados ----- */
+    public List<String> selectAllMetodoPagto() {
+        //Solicita os dados ao DAO
+        return this.pedidoDAO.selectAllMetodoPagto();
+    }
+
+    @Override
+    public List<Pedido> selectAll() {
+        return this.pedidoDAO.selectAll();
+    }
     
+    // public List<Pedido> partialSearch(String nome, String tipoChocolate) {
+    //     //Remove os espaços em branco do início e do fim das strings necessárias
+    //     if(nome != null) nome = nome.stripLeading().stripTrailing();
+        
+    //     //Solicita os dados ao DAO
+    //     return this.pedidoDAO.partialSearch(nome, tipoChocolate);
+    // }
+    
+    // @Override
+    // public List<Pedido> redoLastPartialSearch() {
+    //     //Solicita os dados ao DAO
+    //     return this.pedidoDAO.redoLastPartialSearch();
+    // }
+    
+    // @Override
+    // public boolean insert(Produto produto) {
+    //     //Solita ao DAO a inserção do produto no BD já retornando o resultado
+    //     return this.pedidoDAO.insert(produto);
+    // }
+    
+    // @Override
+    // public boolean update(Produto produto, Produto produtoOriginal) {
+    //     //Solita ao DAO a atualização do produto no BD já retornando o resultado
+    //     return this.pedidoDAO.update(produto, produtoOriginal.getID());
+    // }
+
+    // @Override
+    // public boolean delete(Produto produto) {
+    //     //Solita ao DAO a exclusão do produto no BD já retornando o resultado
+    //     return this.pedidoDAO.delete(produto.getID());
+    // }
 }
