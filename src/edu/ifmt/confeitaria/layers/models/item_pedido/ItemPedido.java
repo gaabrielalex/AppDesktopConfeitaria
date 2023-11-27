7deb22ce402d0bb1d5ba01f44e86f8ab0d19e827
Gabriel <106288896+gaabrielalex@users.noreply.github.com>
2023-11-27 18:32:47 -0400

    .

--- src/edu/ifmt/confeitaria/layers/models/item_pedido/ItemPedido.java
+++ src/edu/ifmt/confeitaria/layers/models/item_pedido/ItemPedido.java
@@ -1,5 +1,20 @@
 package edu.ifmt.confeitaria.layers.models.item_pedido;
 
-public class ItemPedido {
-    
+import java.math.BigDecimal;
+
+import edu.ifmt.confeitaria.layers.models.pedido.Pedido;
+import edu.ifmt.confeitaria.layers.models.produto.Produto;
+import edu.ifmt.confeitaria.util.abstraction_classes.SuperModel;
+
+public class ItemPedido extends SuperModel {
+    private int qtde;
+    private BigDecimal vlrTotalItem;
+    private Pedido pedido;
+    private Produto produto;
+
+    /* Construtor vazio para configurações do DatabaseAcessComponentManager */
+    public ItemPedido() {
+        super(null);
+    }
+
 }
