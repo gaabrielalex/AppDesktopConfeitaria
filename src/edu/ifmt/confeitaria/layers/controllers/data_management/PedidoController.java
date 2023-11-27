49421eb2809981b1437bcec974df9eb6565de75a
Gabriel <106288896+gaabrielalex@users.noreply.github.com>
2023-11-27 14:55:49 -0400

    .

--- src/edu/ifmt/confeitaria/layers/controllers/data_management/PedidoController.java
+++ src/edu/ifmt/confeitaria/layers/controllers/data_management/PedidoController.java
@@ -112,13 +112,12 @@ public class PedidoController extends SuperController<Pedido> {
 
     @Override
     public void modelToFields(Pedido pedido) {
-        //Mapeamento de campos
         this.pedidoView.getEdtCliente().setText(pedido.getCliente().getNome());
         this.pedidoView.getEdtCodCliente().setText(pedido.getCliente().getID().toString());
         this.pedidoView.getDtChooserDtPedido().setDate(pedido.getDtHrPedido());
         this.pedidoView.getDtChooserDtEntrega().setDate(pedido.getDtHrEntrega());
-        this.pedidoView.getEdtVlrTotalPedido().setText(pedido.getVlrTotalPedido().toString());
-        this.pedidoView.getEdtDesconto().setText(pedido.getDesconto().toString());
+        this.pedidoView.getEdtVlrTotalPedido().setText(pedido.getVlrTotalPedido() == null ? "" : pedido.getVlrTotalPedido().toString());
+        this.pedidoView.getEdtDesconto().setText(pedido.getDesconto() == null ? "" : pedido.getDesconto().toString());
         this.pedidoView.getCmbMtdPagto().setSelectedItem(pedido.getMetodoPagto());
         this.pedidoView.getEdtDestinatario().setText(pedido.getNomeDestinatario());
         this.pedidoView.getCkBRetirada().setSelected(pedido.isRetiradaLoja());
