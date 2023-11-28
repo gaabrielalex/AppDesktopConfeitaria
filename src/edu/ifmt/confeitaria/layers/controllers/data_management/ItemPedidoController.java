6f70f1496ca792bd27260f66447b5530c8e207b2
Gabriel <106288896+gaabrielalex@users.noreply.github.com>
2023-11-28 10:15:22 -0400

    .

--- src/edu/ifmt/confeitaria/layers/controllers/data_management/ItemPedidoController.java
+++ src/edu/ifmt/confeitaria/layers/controllers/data_management/ItemPedidoController.java
@@ -37,10 +37,9 @@ public class ItemPedidoController extends SuperController<ItemPedido> {
             pedidoView.getBtnCancelItemPedido(), pedidoView.getBtnRefreshItemPedido(), pedidoView.getTblItemPedido());
     }
 
-    /* O método displey view não faz nada, pois a view em que a entidade do sistema
-    'item pedido' está inserida, é a view do pedido (por se tratar de um mestre detalhe),
-    logo, quem é resposável por exibir a interface do pedido, é a controller da entidade
-    principal desta view, a controller do 'pedido'*/
+    /* O método displey view não faz nada, pois a view em que a entidade do sistema 'item pedido' está
+    inserida, é a view do pedido (por se tratar de um mestre detalhe), logo, quem é resposável por exibir
+    a interface do pedido, é a controller da entidade principal desta view, a controller do 'pedido'*/
     @Override
     public void displayView() {}
     
