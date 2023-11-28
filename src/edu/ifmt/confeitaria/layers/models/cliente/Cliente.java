01081ce9dcf9ae59691f884d09cad0370a499dee
Gabriel <106288896+gaabrielalex@users.noreply.github.com>
2023-11-28 14:55:09 -0400

    .

--- src/edu/ifmt/confeitaria/layers/models/cliente/Cliente.java
+++ src/edu/ifmt/confeitaria/layers/models/cliente/Cliente.java
@@ -62,4 +62,53 @@ public class Cliente extends SuperModel {
     public void setLinkEndereco(String link_endereco) {
         this.linkEndereco = link_endereco;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + ((CPF == null) ? 0 : CPF.hashCode());
+        result = prime * result + ((telefones == null) ? 0 : telefones.hashCode());
+        result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
+        result = prime * result + ((linkEndereco == null) ? 0 : linkEndereco.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Cliente other = (Cliente) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (CPF == null) {
+            if (other.CPF != null)
+                return false;
+        } else if (!CPF.equals(other.CPF))
+            return false;
+        if (telefones == null) {
+            if (other.telefones != null)
+                return false;
+        } else if (!telefones.equals(other.telefones))
+            return false;
+        if (endereco == null) {
+            if (other.endereco != null)
+                return false;
+        } else if (!endereco.equals(other.endereco))
+            return false;
+        if (linkEndereco == null) {
+            if (other.linkEndereco != null)
+                return false;
+        } else if (!linkEndereco.equals(other.linkEndereco))
+            return false;
+        return true;
+    }
 }
