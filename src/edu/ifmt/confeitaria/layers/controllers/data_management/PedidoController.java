3b75f21d6e7767fe848f074b886d7d08160ff606
Gabriel <106288896+gaabrielalex@users.noreply.github.com>
2023-11-28 21:45:56 -0400

    .

--- src/edu/ifmt/confeitaria/layers/controllers/data_management/PedidoController.java
+++ src/edu/ifmt/confeitaria/layers/controllers/data_management/PedidoController.java
@@ -87,6 +87,7 @@ public class PedidoController extends SuperController<Pedido> {
         alterações acima ne um dos fields, logo, o manager entende que estava havendo uma
         operação de atualização, o que não era o caso, eram apenas configurações iniciais*/
         this.pedidoDBCManager.cancel();
+        this.pedidoDBCManager.refresh();
         // ViewUtils.addTextChangeListeners(this.pedidoView.getEdtVlrTotalItemPedido(), this::calculateTotalOrderValue);
         // ViewUtils.addTextChangeListeners(this.pedidoView.getEdtDesconto(), this::calculateTotalOrderValue);
         this.pedidoView.setVisible(true);
