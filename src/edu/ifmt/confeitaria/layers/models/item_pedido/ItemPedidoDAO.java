468edf9c3afcb368963be73fa812e00a3d39f561
Gabriel <106288896+gaabrielalex@users.noreply.github.com>
2023-11-29 02:24:49 -0400

    .

--- src/edu/ifmt/confeitaria/layers/models/item_pedido/ItemPedidoDAO.java
+++ src/edu/ifmt/confeitaria/layers/models/item_pedido/ItemPedidoDAO.java
@@ -149,6 +149,9 @@ public class ItemPedidoDAO {
             e.printStackTrace();
             //Se a inserção não foi realizada com sucesso, retorna false
             return false;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
         } finally {
             //Fecha a conexão com o banco de dados e os recursos criados a partir dela
             DBConnection.closeConnection(statement);
